ec7331dc1e3027824ee18a8069c4f7efffc8334d
Jamie Hewland <jhewland@gmail.com>
2015-10-22 11:55:30 +0200

    Use pytest tmpdir in other tests as well

--- sideloader/tests/test_sideloader.py
+++ sideloader/tests/test_sideloader.py
@@ -23,72 +23,76 @@ class CommandLineTest(object):
     def cmd(self, args, *_args, **kwargs):
         self.cmds.append(args)
 
-    def prefix_cwd(self, path):
-        return '%s/%s' % (os.getcwd(), path)
-
 
 class TestWorkspace(CommandLineTest):
-    def setup_method(self, test_method):
-        super(TestWorkspace, self).setup_method(test_method)
 
+    def _create_workspace(self, tmpdir):
         repo = GitRepo('https://github.com/praekelt/sideloader2.git',
                        'develop', 'sideloader2')
-        self.workspace = Workspace('test_id', 'test_base', '/opt', repo)
-        self.workspace._cmd = self.cmd
 
-    def test_get_path(self):
+        workspace = Workspace('test_id', str(tmpdir), '/opt', repo)
+        workspace._cmd = self.cmd
+        return workspace
+
+    def test_get_path(self, tmpdir):
         """
         Getting a path in the workspace directory returns the correct path.
         """
-        assert (self.workspace.get_path('test') ==
-                self.prefix_cwd('test_base/test_id/test'))
+        workspace = self._create_workspace(tmpdir)
+        assert (workspace.get_path('test') ==
+                str(tmpdir) + '/test_id/test')
 
-    def test_get_package_path(self):
+    def test_get_package_path(self, tmpdir):
         """
         Getting a path in the package directory returns the correct path.
         """
-        assert (self.workspace.get_package_path('test') ==
-                self.prefix_cwd('test_base/test_id/package/test'))
+        workspace = self._create_workspace(tmpdir)
+        assert (workspace.get_package_path('test') ==
+                str(tmpdir) + '/test_id/package/test')
 
-    def test_get_build_path(self):
+    def test_get_build_path(self, tmpdir):
         """
         Getting a path in the build directory returns the correct path.
         """
-        assert (self.workspace.get_build_path('test') ==
-                self.prefix_cwd('test_base/test_id/build/test'))
+        workspace = self._create_workspace(tmpdir)
+        assert (workspace.get_build_path('test') ==
+                str(tmpdir) + '/test_id/build/test')
 
-    def test_get_repo_path(self):
+    def test_get_repo_path(self, tmpdir):
         """
         Getting a path in the repo directory returns the correct path.
         """
-        assert (self.workspace.get_repo_path('test') ==
-                self.prefix_cwd('test_base/test_id/sideloader2/test'))
+        workspace = self._create_workspace(tmpdir)
+        assert (workspace.get_repo_path('test') ==
+                str(tmpdir) + '/test_id/sideloader2/test')
 
-    def test_get_install_path(self):
+    def test_get_install_path(self, tmpdir):
         """
         Getting a path in the install directory returns the correct path.
         """
-        assert (self.workspace.get_install_path('test') ==
-                self.prefix_cwd('test_base/test_id/package/opt/test'))
+        workspace = self._create_workspace(tmpdir)
+        assert (workspace.get_install_path('test') ==
+                str(tmpdir) + '/test_id/package/opt/test')
 
-    def test_fetch_repo(self):
+    def test_fetch_repo(self, tmpdir):
         """
         When the repo is fetched in the workspace, git is called with the
         correct commands.
         """
-        self.workspace.fetch_repo()
+        workspace = self._create_workspace(tmpdir)
+        workspace.fetch_repo()
 
         assert len(self.cmds) == 2
 
         assert (
             self.cmds[0] ==
             ['git', 'clone', 'https://github.com/praekelt/sideloader2.git',
-             self.prefix_cwd('test_base/test_id/sideloader2')]
+             str(tmpdir) + '/test_id/sideloader2']
         )
 
         assert (
             self.cmds[1] ==
-            ['git', '-C', self.prefix_cwd('test_base/test_id/sideloader2'),
+            ['git', '-C', str(tmpdir) + '/test_id/sideloader2',
              'checkout', 'develop']
         )
 
@@ -137,81 +141,82 @@ class TestDeploy(object):
 
 
 class TestBuild(CommandLineTest):
-    def setup_method(self, test_method):
-        """
-        Create the Build and a backing workspace.
-        """
-        super(TestBuild, self).setup_method(test_method)
 
+    def _create_build(self, tmpdir):
         repo = GitRepo('https://github.com/praekelt/sideloader2.git',
                        'develop', 'sideloader2')
-        workspace = Workspace('test_id', 'test_base', '/opt', repo)
+
+        workspace = Workspace('test_id', str(tmpdir), '/opt', repo)
         deploy = Deploy(name='test_deploy', pip=['django', 'pytest'])
         deploy_type = DeployType()
 
-        self.build = Build(workspace, deploy, deploy_type)
-        self.build._cmd = self.cmd
+        build = Build(workspace, deploy, deploy_type)
+        build._cmd = self.cmd
+        return build
 
-    def test_create_build_virtualenv(self):
+    def test_create_build_virtualenv(self, tmpdir):
         """
         When creating a build virtualenv, the virtualenv directory is created,
         pip is upgraded, and the pip dependencies from the Deploy are
         installed.
         """
-        self.build.create_build_virtualenv()
+        build = self._create_build(tmpdir)
+        build.create_build_virtualenv()
 
         assert len(self.cmds) == 4
         assert (
             self.cmds[0] ==
-            ['virtualenv', self.prefix_cwd('test_base/test_id/ve')]
+            ['virtualenv', str(tmpdir) + '/test_id/ve']
         )
 
         assert (
             self.cmds[1] ==
-            [self.prefix_cwd('test_base/test_id/ve/bin/pip'), 'install',
+            [str(tmpdir) + '/test_id/ve/bin/pip', 'install',
              '--upgrade', 'pip']
         )
 
         assert (
             self.cmds[2] ==
-            [self.prefix_cwd('test_base/test_id/ve/bin/pip'), 'install',
+            [str(tmpdir) + '/test_id/ve/bin/pip', 'install',
              '--upgrade', 'django']
         )
 
         assert (
             self.cmds[3] ==
-            [self.prefix_cwd('test_base/test_id/ve/bin/pip'), 'install',
+            [str(tmpdir) + '/test_id/ve/bin/pip', 'install',
              '--upgrade', 'pytest']
         )
 
-    def test_put_env_variables(self):
+    def test_put_env_variables(self, tmpdir):
         """
         When placing the enviornment variables, all the variables are set
         correctly.
         """
-        self.build.put_env_variables()
+        build = self._create_build(tmpdir)
+        build.put_env_variables()
 
-        assert os.getenv('VENV') == self.prefix_cwd('test_base/test_id/ve')
+        assert os.getenv('VENV') == str(tmpdir) + '/test_id/ve'
         assert (os.getenv('PIP') ==
-                self.prefix_cwd('test_base/test_id/ve/bin/pip'))
+                str(tmpdir) + '/test_id/ve/bin/pip')
         assert os.getenv('REPO') == 'sideloader2'
         assert os.getenv('BRANCH') == 'develop'
-        assert os.getenv('WORKSPACE') == self.prefix_cwd('test_base/test_id')
+        assert os.getenv('WORKSPACE') == str(tmpdir) + '/test_id'
         assert (os.getenv('BUILDDIR') ==
-                self.prefix_cwd('test_base/test_id/build'))
+                str(tmpdir) + '/test_id/build')
         assert (os.getenv('INSTALLDIR') ==
-                self.prefix_cwd('test_base/test_id/package/opt'))
+                str(tmpdir) + '/test_id/package/opt')
         assert os.getenv('NAME') == 'test_deploy'
 
-    def test_put_env_variables_path(self):
+    def test_put_env_variables_path(self, tmpdir):
         """
         When placing the environment variables, PATH is prefixed with the
         virtualenv executables (bin) directory.
         """
-        self.build.put_env_variables()
+        build = self._create_build(tmpdir)
+        build.put_env_variables()
 
         assert os.getenv('PATH').startswith(
-            self.prefix_cwd('test_base/test_id/ve/bin'))
+            str(tmpdir) + '/test_id/ve/bin')
 
 
 class TestPackage(CommandLineTest):
